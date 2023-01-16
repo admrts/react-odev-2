21004a6f9d258aaf545c88f5bafb58c8eea736d9
admrts <alidemirtas.7@icloud.com>
2023-01-17 01:26:14 +0300

    done

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="info">
+      <p>Click to edit a todo</p>
+      <p>
+        Created by <a href="https://admrts/">Ali Demirtaş</a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
